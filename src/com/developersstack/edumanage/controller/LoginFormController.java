a887b3fea8df856e49363d753e8d47a22c9b9da5
Prabansha Disanayaka <114307046+Prabansha@users.noreply.github.com>
2025-03-24 10:27:47 +0530

    2

--- src/com/developersstack/edumanage/controller/LoginFormController.java
+++ src/com/developersstack/edumanage/controller/LoginFormController.java
@@ -1,4 +1,33 @@
 package com.developersstack.edumanage.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginFormController {
+    public TextField txtEmail;
+    public PasswordField txtPassword;
+    public AnchorPane context;
+
+    public void forgotPasswordOnActoin(ActionEvent actionEvent) {
+    }
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) {
+
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
+    }
 }

b9350ab2bd64d52ba4cd4b57ff83dafedbc5494e
Prabansha Disanayaka <114307046+Prabansha@users.noreply.github.com>
2025-03-24 15:33:17 +0530

    12

--- src/com/developersstack/edumanage/controller/DashboardFormController.java
+++ src/com/developersstack/edumanage/controller/DashboardFormController.java
@@ -1,10 +1,20 @@
 package com.developersstack.edumanage.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DashboardFormController {
@@ -22,9 +32,29 @@ public class DashboardFormController {
 //        String textDate = dateFormat.format(date);
 //        lblDate.setText(textDate);
         lblDate.setText(new SimpleDateFormat("yyyy/MM/dd").format(new Date()));
+//        lblTime.setText(new SimpleDateFormat("HH:mm:ss").format(new Date()));
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                        e -> {
+                            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+                            lblTime.setText(LocalTime.now().format(dateTimeFormatter));
+                        }
+                ),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
-    public void logOutOnAction(ActionEvent actionEvent) {
+    public void logOutOnAction(ActionEvent actionEvent) throws IOException {
+        setUi("LoginForm");
+    }
+
+    private void setUi(String location) throws IOException {
+        Stage stage = (Stage) context.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+location+".fxml"))));
+        stage.centerOnScreen();
     }
 
     public void signUpOnAction(ActionEvent actionEvent) {
